2cbdcba2dea33623e0464f31fb4cf74ee233ec7e
hyunmin200 <gyejeongjin@gmail.com>
2024-06-22 23:31:53 +0900

    add,feat: css파일 추가 & server.js 파일 등록

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+// 폴더 등록
+app.use(express.static(__dirname + "/src"));
+
 app.listen(8080, () => {
 	console.log("http://localhost:8080 에서 서버 실행중");
 });
@@ -10,7 +13,7 @@ app.get("/", (요청, 응답) => {
 });
 
 app.get("/news", (요청, 응답) => {
-	응답.sendFile(__dirname + "/index.html");
+	응답.sendFile(__dirname + "/src/index.html");
 });
 
 app.get("/shop", (요청, 응답) => {
